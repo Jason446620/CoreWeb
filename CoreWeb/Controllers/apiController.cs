71a0f0db943328ffd93772e7ce21ca8c2451b5b6
Jason446620 <panshubei@outlook.com>
2020-12-14 13:39:38 +0800

    Add project files.

---
+++ CoreWeb/Controllers/apiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWeb.Controllers
+{
+    public class apiController : Controller
+    {
+        public string test1() {
+            return "I'm test api.";
+        }
+    }
+}
